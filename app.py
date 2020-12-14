3fb6be70dc11d255ebdcff4825de006592d6475a
Okie <fauzirachmanokie@gmail.com>
2020-12-14 15:04:44 +0700

    Add Branch Management

--- app.py
+++ app.py
@@ -79,7 +79,65 @@ def update_user_by_id(_id):
                     last_name = row['last_name'], address = row['address'], job = row['job'], source_of_income = row['source_of_income'],
                     phone = row['phone'], last_update = row['last_update'])
 
+#branch management
 
+@app.route('/branch/new', methods = ['POST'])
+def create_new_branch():
+    body = request.json
+    tbbranch_name = body.get('branch_name')
+    tbcity = body.get('city')
+    tbaddress = body.get('address')
+    try:
+        with engine.connect() as connection:
+            qry = text("INSERT INTO branch(branch_name, city, address) VALUES (:branch_name, :city, :address)")
+            connection.execute(qry, branch_name = tbbranch_name, city = tbcity, address = tbaddress)
+            current_insert = text("select * from branch where branch_id = (select max(branch_id) from branch)")
+            result = connection.execute(current_insert)
+            for row in result:
+                return jsonify(branch_id = row['branch_id'], branch_name = row['branch_name'], city = row['city'],
+                    address = row['address'], last_update = row['last_update'])
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/branch/all', methods = ['GET'])
+def get_all_branch():
+    all = []
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT * from branch")
+            result = connection.execute(query)
+            for row in result:
+                all.append({'branch_id' : row['branch_id'], 'branch_name' : row['branch_name'], 'city' : row['city'],
+                'address' : row['address'], 'last_update' : row['last_update']}) 
+            return jsonify(all)
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/branch/<_id>', methods = ['DELETE'])
+def delete_branch_by_id(_id):
+    try:
+        with engine.connect() as connection:
+            query = text("delete from branch where branch_id = :branch_id")
+            connection.execute(query, branch_id = _id)
+            return jsonify(deleted_branch_id = _id)
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/branch/<_id>', methods = ['PUT'])
+def update_branch_by_id(_id):
+    body = request.json
+    tbbranch_name = body.get('branch_name')
+    tbcity = body.get('city')
+    tbaddress = body.get('address')
+    with engine.connect() as connection:
+        query = text("update branch set branch_name = :branch_name, city = :city, address = :address,\
+            last_update = current_timestamp where branch_id = :branch_id")
+        connection.execute(query, branch_id = _id, branch_name = tbbranch_name, city = tbcity, address = tbaddress)
+        view = text("select * from branch where branch_id = :branch_id")
+        result = connection.execute(view, branch_id = _id)
+        for row in result:
+            return jsonify(branch_id = row['branch_id'], branch_name = row['branch_name'], city = row['city'],
+                    address = row['address'], last_update = row['last_update'])
 
 if __name__ == "__main__":
     app.run(debug=True)
