dc40e69a9e2f1da25856803dfb4ed13c66315316
Okie <fauzirachmanokie@gmail.com>
2020-12-14 11:50:17 +0700

    Add update user

--- app.py
+++ app.py
@@ -26,22 +26,58 @@ def user_registration():
             result = connection.execute(current_insert)
             for row in result:
                 return jsonify(user_id = row['user_id'], prefix = row['prefix'], first_name = row['first_name'], 
-                        last_name = row['last_name'], address = row['address'], job = row['job'], source_of_income = row['source_of_income'],
-                        phone = row['phone'])
+                    last_name = row['last_name'], address = row['address'], job = row['job'], source_of_income = row['source_of_income'],
+                    phone = row['phone'], last_update = row['last_update'])
     except Exception as e:
         return jsonify(error=str(e))
 
 @app.route('/user/all', methods = ['GET'])
 def get_all_users():
     all = []
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT * from public.user")
+            result = connection.execute(query)
+            for row in result:
+                all.append({'user_id' : row['user_id'], 'prefix' : row['prefix'], 'first_name' : row['first_name'], 
+                    'last_name' : row['last_name'], 'address' : row['address'], 'job' : row['job'],
+                    'source_of_income' : row['source_of_income'], 'phone' : row['phone'], 'last_update' : row['last_update'] }) 
+            return jsonify(all)
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/user/<_id>', methods = ['DELETE'])
+def delete_user_by_id(_id):
+    try:
+        with engine.connect() as connection:
+            query = text("delete from public.user where user_id = :user_id")
+            connection.execute(query, user_id = _id)
+            return jsonify(deleted_user_id = _id)
+    except Exception as e:
+        return jsonify(error=str(e))
+
+@app.route('/user/<_id>', methods = ['PUT'])
+def update_user_by_id(_id):
+    body = request.json
+    tbprefix = body.get('prefix')
+    tbfirst_name = body.get('first_name')
+    tblast_name = body.get('last_name')
+    tbaddress = body.get('address')
+    tbjob = body.get('job')
+    tbsource_of_income = body.get('source_of_income')
+    tbphone = body.get('phone')
     with engine.connect() as connection:
-        query = text("SELECT * from public.user")
-        result = connection.execute(query)
+        query = text("update public.user set prefix = :prefix, first_name = :first_name, last_name = :last_name,\
+		    address = :address, job = :job, source_of_income = :source_of_income, phone = :phone,\
+            last_update = current_timestamp where user_id = :user_id")
+        connection.execute(query, user_id = _id, prefix = tbprefix, first_name = tbfirst_name, last_name = tblast_name, 
+            address = tbaddress, job = tbjob, source_of_income = tbsource_of_income, phone = tbphone)
+        view = text("select * from public.user where user_id = :user_id")
+        result = connection.execute(view, user_id = _id)
         for row in result:
-            all.append({'user_id' : row['user_id'], 'prefix' : row['prefix'], 'first_name' : row['first_name'], 
-                        'last_name' : row['last_name'], 'address' : row['address'], 'job' : row['job'],
-                        'source_of_income' : row['source_of_income'], 'phone' : row['phone'] }) 
-        return jsonify(all)
+            return jsonify(user_id = row['user_id'], prefix = row['prefix'], first_name = row['first_name'], 
+                    last_name = row['last_name'], address = row['address'], job = row['job'], source_of_income = row['source_of_income'],
+                    phone = row['phone'], last_update = row['last_update'])
 
 
 
